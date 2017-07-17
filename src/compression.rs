f61165e1f4132a56e6bd3a66ebbf0afe981e74f7
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-16 21:40:30 -0500

    Implemented brotli compression

--- src/compression.rs
+++ src/compression.rs
@@ -1,3 +1,4 @@
+extern crate brotli2;
 extern crate flate2;
 
 use ::*;
@@ -22,6 +23,7 @@ impl DefaultFileCompressor for CompressionAlgorithm {
     fn compress(&self, src: &Path, dst: &Path) -> Result<()> {
         match self {
             &CompressionAlgorithm::GZip => gzip_compress(src, dst),
+            &CompressionAlgorithm::Brotli => brotli_compress(src, dst),
             _ => bail!("Compression algorithm not implemented!")
         }
     }
@@ -37,11 +39,26 @@ fn gzip_compress(src_path: &Path, dst_path: &Path) -> Result<()> {
        let bytes_read = src.read(&mut buf).chain_err(|| "Error reading from source file!")?;
        match bytes_read {
            0 => break, //end of file
-           l => l
+           l => encoder.write_all(&buf[0..l]).chain_err(|| "Fatal gzip encoder error!")?,
        };
-
-       encoder.write_all(&buf[0..bytes_read]).chain_err(|| "Fatal gzip encoder error!")?;
    }
 
    Ok(())
+}
+
+fn brotli_compress(src_path: &Path, dst_path: &Path) -> Result<()> {
+    let mut src = File::open(src_path)?;
+    let dst = File::create(dst_path)?;
+
+    let mut buf = [0u8; 1024];
+    let mut encoder = brotli2::write::BrotliEncoder::new(dst, 6);
+    loop {
+        let bytes_read = src.read(&mut buf).chain_err(|| "Error reading from source file!")?;
+        match bytes_read {
+            0 => break, //end of file
+            l => encoder.write_all(&buf[0..l]).chain_err(|| "Fatal gzip encoder error!")?,
+        };
+    }
+
+    Ok(())
 }
