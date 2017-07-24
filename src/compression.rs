0ff385ed91fc7ebf3bfb70cd54881910b2921036
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-23 20:43:08 -0500

    Implemented --quality for gzip

--- src/compression.rs
+++ src/compression.rs
@@ -23,7 +23,7 @@ impl CompressionFormat for CompressionAlgorithm {
 impl FileCompressor for CompressionAlgorithm {
     fn compress(&self, src: &Path, dst: &Path, quality: Option<u8>) -> Result<()> {
         match self {
-            &CompressionAlgorithm::GZip => gzip_compress(src, dst),
+            &CompressionAlgorithm::GZip => gzip_compress(src, dst, quality),
             &CompressionAlgorithm::Brotli => brotli_compress(src, dst),
             &CompressionAlgorithm::WebP => webp_compress(src, dst, quality),
             &CompressionAlgorithm::Zopfli => zopfli_compress(src, dst),
@@ -32,12 +32,19 @@ impl FileCompressor for CompressionAlgorithm {
     }
 }
 
-fn gzip_compress(src_path: &Path, dst_path: &Path) -> Result<()> {
+fn gzip_compress(src_path: &Path, dst_path: &Path, quality: Option<u8>) -> Result<()> {
     let mut src = File::open(src_path)?;
     let dst = File::create(dst_path)?;
 
+    let level = match quality {
+        Some(0) => flate2::Compression::None,
+        Some(1) => flate2::Compression::Fast,
+        Some(9) => flate2::Compression::Best,
+        _ => flate2::Compression::Default,
+    };
+
+    let mut encoder = flate2::write::GzEncoder::new(dst, level);
     let mut buf = [0u8; 1024];
-    let mut encoder = flate2::write::GzEncoder::new(dst, flate2::Compression::Default);
     loop {
         let bytes_read = src.read(&mut buf).chain_err(|| "Error reading from source file!")?;
         match bytes_read {
